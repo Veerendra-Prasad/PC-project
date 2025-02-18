fe215d3cb5009bf12c97a9a8162bc81128a66d5f
Veerendra Prasad <veerendraprasadkarnam27@gmail.com>
2025-02-18 17:51:47 +0530

    fix the issue with the darkmode

--- comment/createNewComment.js
+++ comment/createNewComment.js
@@ -1,7 +1,7 @@
 import {
   createNewComment,
   getDataById,
-  randomId
+  randomId,
 } from "../database/database.js";
 
 const urlData = new URLSearchParams(window.location.search).get("data");
@@ -9,17 +9,27 @@ const input = document.querySelector(".input");
 const AuthorInput = document.querySelector(".input-author");
 const button = document.querySelector(".button");
 
+const url = new URLSearchParams(window.location.search).get("mode");
+if (url != null) {
+  if (url === "light") {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+}
+
 button.addEventListener("click", async () => {
   const parsedData = getDataById(urlData);
   const data = {
-    id : randomId(),
+    id: randomId(),
     replying: parsedData.author,
     author: AuthorInput.value || "Anonymous",
     description: input.value,
   };
   const result = await createNewComment(data, urlData);
+  const url = new URLSearchParams(window.location.search).get('mode');
   if (result == "done") {
-    window.location.href = "../index.html";
+    window.location.href = `../index/index.html?mode=${url}`;
   } else if (result == "error") {
     alert("Error while creating a comment");
   }
